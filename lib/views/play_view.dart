7b5339361fd39e6472ccec0aab862bf88ebc608e
Yavuz Selim Şerifoğlu <selimserifoglu03@gmail.com>
2021-10-26 10:03:45 +0300

    style of page view ended

--- lib/views/play_view.dart
+++ lib/views/play_view.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:spotify_clone/components/play/play_view_app_bar.dart';
+import 'package:spotify_clone/constants/text_styles_constants.dart';
 
 class PlayView extends StatefulWidget {
   PlayView({Key? key}) : super(key: key);
@@ -20,9 +21,12 @@ class _PlayViewState extends State<PlayView> {
         padding: EdgeInsets.all(30.0),
         child: Column(
           children: [
-            SizedBox(
+            Container(
+              margin: EdgeInsets.only(bottom: 20.0),
               height: 400.0,
-              child: Image.asset('assets/images/japanese.jpg',),
+              child: Image.asset(
+                'assets/images/japanese.jpg',
+              ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -30,27 +34,52 @@ class _PlayViewState extends State<PlayView> {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Japanese Lofi'),
-                    Text('Iration')
+                    Text(
+                      'Japanese Lofi',
+                      style: sectionTitleTextStyle,
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Text('Iration',
+                    style: detailTextStyle,)
                   ],
                 ),
-                IconButton(onPressed: null, icon: Icon(Icons.favorite_border))
+                IconButton(
+                    onPressed: null,
+                    icon: Icon(
+                      Icons.favorite_border,
+                      color: Colors.white,
+                    ))
               ],
+            ),
+            SizedBox(
+              height: 30.0,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                IconButton(onPressed: null, icon: Icon(Fontisto.step_backwrad)),
-                IconButton(iconSize: 70,onPressed: null, icon: Icon(MaterialCommunityIcons.pause_circle,)),
-                IconButton(onPressed: null, icon: Icon(Fontisto.step_forward))
+                IconButton(
+                    iconSize: 30,
+                    onPressed: null,
+                    icon: Icon(
+                      Fontisto.step_backwrad,
+                      color: Colors.white,
+                    )),
+                IconButton(
+                    iconSize: 90,
+                    onPressed: null,
+                    icon: Icon(MaterialCommunityIcons.pause_circle,
+                        color: Colors.white)),
+                IconButton(
+                    iconSize: 30,
+                    onPressed: null,
+                    icon: Icon(Fontisto.step_forward, color: Colors.white))
               ],
             )
-
           ],
         ),
       ),
     );
   }
 }
-
-
